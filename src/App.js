2c77b10f1d77201d16270495608a7af09e577708
Harish Andey <iharishnaidu@gmail.com>
2021-10-20 02:47:24 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,74 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {useState} from "react";
+import {Button} from "react-bootstrap";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+
+    const [formData, setFormData] = useState([{ title: "", caption : ""}])
+
+    let handleChange = (i, e) => {
+        let newFormValues = [...formData];
+        newFormValues[i][e.target.name] = e.target.value;
+        setFormData(newFormValues);
+        validate(i, newFormValues[i][e.target.name]);
+    }
+
+    let validate = (i, data) => {
+        let element = document.getElementById(i);
+        if (isNaN(data))
+            element.parentElement.classList.add("error");
+        else
+            element.parentElement.classList.remove("error");
+    }
+
+    let addControls = () => {
+        setFormData([...formData, { title: "", caption: "" }])
+    }
+
+    let removeControls = (i) => {
+        let newFormValues = [...formData];
+        newFormValues.splice(i, 1);
+        setFormData(newFormValues)
+    }
+
+    let submit = (event) => {
+        event.preventDefault();
+        console.log(JSON.stringify(formData));
+    }
+
+    return (
+        <>
+            <div className="wrapper">
+                <form className="form-container"  onSubmit={submit}>
+                    {formData.map((element, index) => (
+                        <div className="form-row" key={index}>
+                            <div className="form-group">
+                                <input className="form-control" id={index} placeholder="Title" type="text" name="title" value={element.title || ""} onChange={e => handleChange(index, e)} />
+                            </div>
+                            <div className="form-group">
+                                <input className="form-control" id={index} placeholder="Caption" type="text" name="caption" value={element.caption || ""} onChange={e => handleChange(index, e)} />
+                            </div>
+                            <div className="form-group">
+                                {
+                                    index ?
+                                        <Button variant="primary" onClick={() => removeControls(index)}>Remove</Button>
+                                        : null
+                                }
+                            </div>
+                        </div>
+                    ))}
+                    <div className="form-row">
+                        <div className="form-group">
+                            <Button variant="primary" type="button" onClick={() => addControls()}>Add</Button>
+                        </div>
+                        <div className="form-group">
+                            <Button variant="primary" type="submit">Submit</Button>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </>
+    );
 }
 
 export default App;
